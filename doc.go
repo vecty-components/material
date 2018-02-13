c2e4524ceb1a0375f6db3fbb66e87fdc0a2cb5d8
bzub <bzubrod@gmail.com>
2018-02-12 20:39:21 -0600

    Move package documentation to doc.go

---
+++ doc.go
@@ -0,0 +1,37 @@
+/*
+The material package provides interfaces and functions needed to implement/run
+material components in GopherJS.
+
+Quickstart Guide
+
+1. In your project include the all-in-one distribution of the MDC javascript
+library and set it to the global variable "mdc". This can be done a number of
+ways (HTML script element, webpack, filename "mdc.inc.js" for gopherjs to pick
+up, etc).
+
+2. Import a Material component from this project in your Go progrem.
+
+	import "agamigo.io/material"
+	import "agamigo.io/material/checkbox"
+
+3. Make the HTML suitable for that MDC component available to your GopherJS
+program. See: https://material.io/components/web/catalog/
+
+  <html>
+    <body>
+      <div class="mdc-checkbox">
+        <input class="mdc-checkbox__native-control" type="checkbox">
+      </div>
+    </body>
+  </html>
+
+4. Put that HTMLElement into a GopherJS object.
+
+	cbElem := js.Global.Get("document").Get("body").Get("firstElementChild")
+
+5. Create a new instance of the component and start it.
+
+	cb := checkbox.CB{}
+	material.Start(cb, cbElem)
+*/
+package material // import "agamigo.io/material"
