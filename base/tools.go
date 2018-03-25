e163d81d964cb279253b2b38c5b8f1c9a1dc3fd4
bzub <bzubrod@gmail.com>
2018-03-25 18:28:50 -0500

    base: Expose StaticComponent type
    So components can detect it with a type switch.

--- base/tools.go
+++ base/tools.go
@@ -5,30 +5,30 @@ import (
 	"github.com/gopherjs/vecty/elem"
 )
 
-type childComponent struct {
+type StaticComponent struct {
 	vecty.Core
-	child vecty.ComponentOrHTML
+	Child vecty.ComponentOrHTML
 }
 
 // RenderStoredChild is a helper which provides a Component which wraps the
 // provided ComponentOrHTML. It exists as a workaround to a vecty issue.
 //
 // See: https://github.com/gopherjs/vecty/issues/191
-func RenderStoredChild(child vecty.ComponentOrHTML) *childComponent {
-	return &childComponent{child: child}
+func RenderStoredChild(child vecty.ComponentOrHTML) *StaticComponent {
+	return &StaticComponent{Child: child}
 }
 
-func (s *childComponent) Render() vecty.ComponentOrHTML {
-	switch t := s.child.(type) {
+func (c *StaticComponent) Render() vecty.ComponentOrHTML {
+	switch t := c.Child.(type) {
 	case vecty.List:
 		return elem.Div(t)
 	}
-	return s.child
+	return c.Child
 }
 
-func (s *childComponent) SkipRender(prev vecty.Component) bool {
+func (c *StaticComponent) SkipRender(prev vecty.Component) bool {
 	switch prev.(type) {
-	case *childComponent:
+	case *StaticComponent:
 		return true
 	}
 	return false
