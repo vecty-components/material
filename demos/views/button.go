cc7c6756e5f327dc935f496064569f3e509cdf33
xoviat <xoviat@gmail.com>
2021-10-01 12:03:23 -0500

    add demo buttons

--- demos/views/button.go
+++ demos/views/button.go
@@ -3,7 +3,9 @@ package views
 import (
 	"github.com/hexops/vecty"
 	"github.com/hexops/vecty/elem"
+	"github.com/vecty-material/material/button"
 	"github.com/vecty-material/material/demos/components"
+	"github.com/vecty-material/material/icon"
 )
 
 func NewButtonPage() *components.ComponentCatalogPanel {
@@ -24,15 +26,55 @@ type ButtonHero struct {
 }
 
 func (bh *ButtonHero) Render() vecty.ComponentOrHTML {
-	return elem.Div()
+	return &button.B{
+		Root: vecty.Markup(
+			vecty.Class("hero-button"),
+		),
+	}
 }
 
 type ButtonDemos struct {
 	vecty.Core
 }
 
+func (bd *ButtonDemos) renderButtonVariant(title, variantClass string) vecty.ComponentOrHTML {
+	return elem.Div(
+		elem.Heading3(
+			vecty.Markup(
+				vecty.Class("mdc-typography--subtitle1"),
+			),
+		),
+		&button.B{
+			Root: vecty.Markup(
+				vecty.Class("demo-button", variantClass),
+			),
+			Label: vecty.Text("Default"),
+		},
+		&button.B{
+			Root: vecty.Markup(
+				vecty.Class("demo-button", variantClass),
+			),
+			Label: vecty.Text("Dense"),
+			Dense: true,
+		},
+		&button.B{
+			Root: vecty.Markup(
+				vecty.Class("demo-button", variantClass),
+			),
+			Label: vecty.Text("Icon"),
+			Icon: &icon.I{
+				Name: "favorite",
+			},
+		},
+	)
+}
+
 func (bd *ButtonDemos) Render() vecty.ComponentOrHTML {
 	return elem.Div(
-		vecty.Text("button demos"),
+		// bd.renderButtonVariant("Text Button", ""),
+		bd.renderButtonVariant("Raised Button", "mdc-button--raised"),
+		bd.renderButtonVariant("Unelevated Button", "mdc-button--unelevated"),
+		bd.renderButtonVariant("Outlined Button", "mdc-button--outlined"),
+		// bd.renderButtonVariant("Shaped Button", "mdc-button--unelevated demo-button-shaped"),
 	)
 }
